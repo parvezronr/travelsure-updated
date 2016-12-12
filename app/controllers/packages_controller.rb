0ad435a291ae8cf8d2b2b0b7834f20fc12048d41
parvezronr <syedparvezronr.com>
2016-12-12 19:02:39 +0530

    dsfds

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -5,7 +5,7 @@ class PackagesController < ApplicationController
   # GET /packages.json
   def index
     #binding.pry
-    @packages = current_user.agency.packages
+    @packages = Package.all
   end
 
   # GET /packages/1
